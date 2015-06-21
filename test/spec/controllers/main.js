3212fb3038c33bb17413cf8a9b53a0f3df033660
Javier Tello <jtelloal@gmail.com>
2015-06-21 23:49:23 +0200

    Karma tests added

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,27 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('poste should be null before start and invalido == false', function () {
+    expect(scope.poste).toBe(null);
+		expect(scope.invalido).toBe(false);
+		expect(scope.url).toBe('http://www.urbanosdezaragoza.es/frm_esquemaparadatime.php?poste=null');
   });
+	
+	// Add a correct "poste" and obtain info
+	it('invalido should be false and url correct', function () {
+		scope.poste = 222;
+	  scope.obtener();
+    expect(scope.poste).toBe(222);
+		expect(scope.invalido).toBe(false);
+		expect(scope.url).toBe('http://www.urbanosdezaragoza.es/frm_esquemaparadatime.php?poste=222');
+  });
+
+	// Add a incorrect "poste" and obtain info
+	it('invalido should be true and url incorrect', function () {
+		scope.poste = 999999999;
+		scope.obtener();
+    expect(scope.poste).toBe(999999999);
+		expect(scope.invalido).toBe(true);
+  });
+	
 });
