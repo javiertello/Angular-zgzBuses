e656de5f349185ff0fae806496f238dd7ca16b7f
Javier Tello <jtelloal@gmail.com>
2015-06-21 18:44:44 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Horarios Urbanos de Zaragoza
+
+A **responsive** WebApp to get the times of the buses in Zaragoza, developed with [AngularJS](https://angularjs.org/)
+
+This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
+version 0.11.1.
+
+## Build & development
+
+Run `grunt` for building and `grunt serve` for preview.
+WebApp is at [http://localhost:9000/#/](http://localhost:9000/#/)
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tareasApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tareasApp
+ */
+angular.module('tareasApp')
+  .controller('MainCtrl', function ($scope, $sce) {
+  	var urlBase = 'http://www.urbanosdezaragoza.es/frm_esquemaparadatime.php?poste=';
+  	
+  	$scope.poste = null;
+  	$scope.url = urlBase + $scope.poste;
+
+  	$scope.trustSrc = function(src) {
+   		return $sce.trustAsResourceUrl(src);
+    }
+  	$scope.invalido = false;
+
+  	$scope.obtener = function (){
+  		if(!angular.isNumber($scope.poste)){
+  			$scope.invalido = true;
+  		} else{
+  			if($scope.poste > 0 && $scope.poste < 99999){
+  				$scope.invalido = false;
+  				$scope.url = urlBase + $scope.poste;
+  			} else{
+  				$scope.invalido = true;
+  			}
+  		}
+  	}
+  });
